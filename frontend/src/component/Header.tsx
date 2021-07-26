f7a6fc63d72ca3c34cb488b2731b7dcff84f3c74
Karl <jae637@naver.com>
2021-07-27 01:23:12 +0900

    init front template

---
+++ frontend/src/component/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="header">
+            <header className="App-header">
+               header
+            </header>
+        </div>
+    );
+}
+
+export default App;
