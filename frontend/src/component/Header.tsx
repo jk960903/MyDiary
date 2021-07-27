6f1badbac5f327216d6f8840a97459cf9bb058db
Karl <jae637@naver.com>
2021-07-27 19:12:06 +0900

    header 제작 완료, title 변경

--- frontend/src/component/Header.tsx
+++ frontend/src/component/Header.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+} from 'reactstrap';
+import LoginModal from './LoginModal'
 
-function App() {
-    return (
-        <div className="header">
-            <header className="App-header">
-               header
-            </header>
-        </div>
-    );
+export default class Header extends React.Component{
+    render(){
+        return (
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/" className="ms-4">Title</NavbarBrand>
+                    <Nav className="ms-2" navbar>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav>
+                                Options
+                            </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem href="/sub">
+                                    Option 1
+                                </DropdownItem>
+                                <DropdownItem>
+                                    Option 2
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                    <Nav className="ms-auto me-4">
+                        <LoginModal/>
+                    </Nav>
+                </Navbar>
+            </div>
+        );
+    }
 }
-
-export default App;
