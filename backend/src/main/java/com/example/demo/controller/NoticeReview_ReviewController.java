ca5ebe81d89534696dff53751189e8c510c4b2bd
jk960903 <jk960903@naver.com>
2021-08-03 02:42:12 +0900

    git 오류 확인용 변경

--- backend/src/main/java/com/example/demo/controller/NoticeReview_ReviewController.java
+++ backend/src/main/java/com/example/demo/controller/NoticeReview_ReviewController.java
@@ -1,3 +1,8 @@
 package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value="/NoticeReviewReview")
 public class NoticeReview_ReviewController {
+
 }
