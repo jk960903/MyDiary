c97374365aba3e19b8ac5131809f21bec7c477df
Karl <jae637@naver.com>
2021-09-04 18:15:35 +0900

    api base url 변경

--- frontend/src/apis/LoginApi.tsx
+++ frontend/src/apis/LoginApi.tsx
@@ -4,7 +4,7 @@ const url = "http://localhost:8080";
 
 export default {
     login(params:object){
-        return axios.get("/api/loginAction",params)
+        return axios.get(url+"/api/loginAction",params)
             .then(response=>{
                 let data= response.data;
                 return data
